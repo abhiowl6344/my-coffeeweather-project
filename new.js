074a6e6b56f8268da7e8094d832470cc759de38c
abhiowl6344 <86313610+abhiowl6344@users.noreply.github.com>
2022-07-25 14:05:20 +1000

    Added Search Form

--- new.js
+++ new.js
@@ -43,8 +43,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "4ea4b2990fe44ae6989b05ed8dbb0004";
-let city = "Shillong";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "4ea4b2990fe44ae6989b05ed8dbb0004";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let city = "Sydney";
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
