956cdc61f3df1bed5476f4bb91336712572f112a
abhiowl6344 <86313610+abhiowl6344@users.noreply.github.com>
2022-07-29 23:41:13 +1000

    Transfer HTML data to JS for looping

--- new.js
+++ new.js
@@ -21,6 +21,30 @@ function formateDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+    <div class="weather-forecast-date"> ${day} </div>
+    <img src="http://openweathermap.org/img/wn/09d@2x.png" alt="" width="40" />
+    <div class="weather-forecast-temperature">
+      <span class="weather-forecast-temperature-min"> 08°</span>
+      <span class="weather-forecast-temperature-max"> 12° </span>
+    </div>
+  </div>
+    
+  `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -82,3 +106,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displaycelsiusTemperature);
 
 let city = "Sydney";
+displayForecast();
